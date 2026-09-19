c88fa4c47a206a5644ec82107bad54802f61d26d
Franaffel <franksbass19@gmail.com>
2026-09-20 02:11:37 +0800

    u

--- _st.py
+++
@@ -1,5 +0,0 @@
-import io
-p = r"c:/Autolab/auto-lab/Testing/_selftest_builder.gd"
-lines = io.open(p, encoding="utf-8").read().split("\n")
-for i in range(95, min(155, len(lines))):
-    print("%4d| %s" % (i + 1, lines[i].rstrip("\r")))
